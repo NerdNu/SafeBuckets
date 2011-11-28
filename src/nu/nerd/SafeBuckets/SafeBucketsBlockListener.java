79fba1011c1bc438e09fd15637843878ed3c5594
Ed Kellett <ed@estc.demon.co.uk>
2011-11-28 20:09:05 +0000

    made the plugin actually work!

--- src/nu/nerd/SafeBuckets/SafeBucketsBlockListener.java
+++ src/nu/nerd/SafeBuckets/SafeBucketsBlockListener.java
@@ -4,6 +4,8 @@ import org.bukkit.Material;
 import org.bukkit.event.block.BlockListener;
 import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.block.BlockFromToEvent;
 
 public class SafeBucketsBlockListener extends BlockListener {
     private SafeBuckets plugin;
@@ -18,11 +20,29 @@ public class SafeBucketsBlockListener extends BlockListener {
             return;
 
         Material mat = event.getBlock().getType();
-        if (mat == Material.STATIONARY_LAVA || mat == Material.STATIONARY_WATER) {
+            if (mat == Material.STATIONARY_LAVA || mat == Material.STATIONARY_WATER) {
             if (plugin.bucketBlocks.contains(event.getBlock().hashCode())) {
                 plugin.log.info("cancelling");
+                //event.getBlock().getRelative(BlockFace.UP).setType(Material.BRICK);
                 event.setCancelled(true);
             }
+        } else if (mat == Material.LAVA || mat == Material.WATER) {
+            if (plugin.bucketBlocks.contains(event.getBlock().hashCode())) {
+                plugin.log.info("block at " + event.getBlock().getLocation().toString() + " turned into flowing water!");
+                //event.getBlock().getRelative(BlockFace.UP).setType(Material.DIAMOND_BLOCK);
+                //event.getBlock().setTypeId(event.getBlock().getTypeId() + 1);
+            }
+        }
+    }
+    
+    @Override
+    public void onBlockFromTo(BlockFromToEvent event) {
+        if (event.isCancelled())
+            return;
+        
+        if (plugin.bucketBlocks.contains(event.getBlock().hashCode())) {
+            plugin.log.info("fromto called by " + event.getBlock().getLocation().toString());
+            event.setCancelled(true);
         }
     }
 
