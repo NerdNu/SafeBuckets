ee1838a136c67c4f93b1788b43d9c38006aab425
totemo <TotemoResearch@gmail.com>
2017-02-07 08:07:30 +1030

    Bug fixes.
     * Fix safe water flowing when replacing single block thick snow under a
       block with a non-zero data value.
     * Fix unsafe water buckets not automatically refilling themselves (workaround
       for https://hub.spigotmc.org/jira/projects/SPIGOT/issues/SPIGOT-2560).
     * Run Eclipse auto-formatter.

--- src/nu/nerd/SafeBuckets/SafeBucketsListener.java
+++ src/nu/nerd/SafeBuckets/SafeBucketsListener.java
@@ -1,11 +1,10 @@
 package nu.nerd.SafeBuckets;
 
-import com.sk89q.worldguard.LocalPlayer;
-import com.sk89q.worldguard.protection.ApplicableRegionSet;
-import com.sk89q.worldguard.protection.managers.RegionManager;
-import me.sothatsit.usefulsnippets.EnchantGlow;
+import java.util.Date;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
@@ -21,11 +20,20 @@ import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.BlockFromToEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
+import org.bukkit.event.player.PlayerBucketFillEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.material.Dispenser;  //> Material because we need the getFacing method (DirectionalContainer.class)
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.material.Dispenser; //> Material because we need the getFacing method (DirectionalContainer.class)
 
-import java.util.Date;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+
+import me.sothatsit.usefulsnippets.EnchantGlow;
 
 public class SafeBucketsListener implements Listener {
 
@@ -56,46 +64,50 @@ public class SafeBucketsListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockDispense(BlockDispenseEvent event) {
-    	if (event.getBlock().getState().getData() instanceof Dispenser) {
-	        Material mat = event.getItem().getType();
-	        Dispenser dispenser = (Dispenser)event.getBlock().getState().getData();
-	    	Block blockDispenser = event.getBlock();
+        if (event.getBlock().getState().getData() instanceof Dispenser) {
+            Material mat = event.getItem().getType();
+            Dispenser dispenser = (Dispenser) event.getBlock().getState().getData();
+            Block blockDispenser = event.getBlock();
             Block blockDispensed = blockDispenser.getRelative(dispenser.getFacing());
 
-	        if (mat == Material.LAVA_BUCKET || mat == Material.WATER_BUCKET) {
+            if (mat == Material.LAVA_BUCKET || mat == Material.WATER_BUCKET) {
                 if (plugin.getConfig().getBoolean("dispenser.enabled")) {
                     if (plugin.getConfig().getBoolean("dispenser.safe") && plugin.isSafeLiquid(blockDispenser)) {
                         plugin.addBlockToCacheAndDB(blockDispensed);
                     }
 
-                    plugin.debug("SafeBuckets: Dispense " + Util.formatCoords(event.getBlock()) + (plugin.isSafeLiquid(event.getBlock()) ? " safe" : " unsafe"));
-	        	} else {
-	        		event.setCancelled(true);
-	        	}
-	        } else if (mat == Material.BUCKET) {
+                    plugin.debug("SafeBuckets: Dispense " + Util.formatCoords(event.getBlock())
+                                 + (plugin.isSafeLiquid(event.getBlock()) ? " safe" : " unsafe"));
+                } else {
+                    event.setCancelled(true);
+                }
+            } else if (mat == Material.BUCKET) {
                 if (blockDispensed.getType() == Material.WATER || blockDispensed.getType() == Material.STATIONARY_WATER ||
-                    blockDispensed.getType() == Material.LAVA  || blockDispensed.getType() == Material.STATIONARY_LAVA) {
+                    blockDispensed.getType() == Material.LAVA || blockDispensed.getType() == Material.STATIONARY_LAVA) {
                     // Empty bucket taking liquid.
                     if (plugin.getConfig().getBoolean("dispenser.enabled")) {
-                        // Regardless of whether the dispenser is safe or unsafe, when liquid in front of it
-                        // is removed from the world, it is removed from the DB (i.e. made to flow) too.
+                        // Regardless of whether the dispenser is safe or
+                        // unsafe, when liquid in front of it is removed from
+                        // the world, it is removed from the DB (i.e. made to
+                        // flow) too.
                         plugin.removeSafeLiquidFromCacheAndDB(blockDispensed);
 
-                        plugin.debug("SafeBuckets: Un-Dispense " + Util.formatCoords(event.getBlock()) + (plugin.isSafeLiquid(event.getBlock()) ? " safe" : " unsafe"));
+                        plugin.debug("SafeBuckets: Un-Dispense " + Util.formatCoords(event.getBlock())
+                                     + (plugin.isSafeLiquid(event.getBlock()) ? " safe" : " unsafe"));
                     } else {
                         event.setCancelled(true);
                     }
                 }
-	        }
-		}
+            }
+        }
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockFromTo(BlockFromToEvent event) {
         Block block = event.getBlock();
-        //if (plugin.table.isSafeLiquid(event.getBlock())) {
+        // if (plugin.table.isSafeLiquid(event.getBlock())) {
         if (plugin.isSafeLiquid(event.getBlock())) {
-            //somehow our block got changed to flowing, change it back
+            // somehow our block got changed to flowing, change it back
             if (block.getType() == Material.WATER) {
                 block.setTypeId(9, false);
             }
@@ -112,7 +124,8 @@ public class SafeBucketsListener implements Listener {
         }
     }
 
-    // Stop all ice melting, putting every melted ice block in the database would very quickly fill it to excessive sizes
+    // Stop all ice melting, putting every melted ice block in the database
+    // would very quickly fill it to excessive sizes
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onBlockFade(BlockFadeEvent event) {
         if (event.getBlock().getType() == Material.ICE) {
@@ -125,12 +138,12 @@ public class SafeBucketsListener implements Listener {
         Block block = event.getBlock();
         if (block.getType() == Material.ICE) {
             if (!event.getPlayer().getItemInHand().containsEnchantment(Enchantment.SILK_TOUCH)) {
-                // If we are breaking the block with an enchanted pick then don't replace it with air, we want it to drop as an item
-                //event.getBlock().setTypeId(0);
+                // If we are breaking the block with an enchanted pick then
+                // don't replace it with air, we want it to drop as an item
+                // event.getBlock().setTypeId(0);
                 plugin.addBlockToCacheAndDB(block);
             }
-        }
-        else if (block.getType() == Material.DISPENSER) {
+        } else if (block.getType() == Material.DISPENSER) {
             plugin.removeSafeLiquidFromCacheAndDB(block);
         }
     }
@@ -142,38 +155,59 @@ public class SafeBucketsListener implements Listener {
         // Someone is using liquid to replace this block, staff making it flow
         if (block.isLiquid() || (!block.isLiquid() && plugin.isSafeLiquid(block))) {
             plugin.removeSafeLiquidFromCacheAndDB(block);
-        }
-        else if (block.getType() == Material.DISPENSER) {
+        } else if (block.getType() == Material.DISPENSER) {
             plugin.addBlockToCacheAndDB(block);
         }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onPlayerBucketEmpty(PlayerBucketEmptyEvent event) {
-        Block block = event.getBlockClicked().getRelative(event.getBlockFace());
+        Player player = event.getPlayer();
+        Block block = event.getBlockClicked();
 
-        if (event.getBlockClicked().getType().equals(Material.SNOW)) {
-            // Handle single snow layers, which are removed when water is placed, resulting in off-by-one
-            if (block.getData() == 0x0) {
-                block = event.getBlockClicked();
-            }
+        // Handle single snow layers, which are removed when water is
+        // placed, resulting in off-by-one
+        boolean isSingleLayerSnow = (block.getType() == Material.SNOW && block.getData() == 0);
+        if (!isSingleLayerSnow) {
+            block = block.getRelative(event.getBlockFace());
         }
 
-    	if (plugin.getConfig().getBoolean("bucket.enabled")) {
-        	if (plugin.getConfig().getBoolean("bucket.safe")) {
-				ItemStack itemInHand = event.getPlayer().getItemInHand();
-				if (!EnchantGlow.hasGlow(itemInHand)) {
-					plugin.addBlockToCacheAndDB(block);
-				} else {
-					plugin.removeSafeLiquidFromCacheAndDB(block);
-					if (event.getPlayer().hasPermission("safebuckets.tools.norefill")) {
-						event.setItemStack(itemInHand);
-					}
-				}
-        	}
-    	} else {
-    		event.setCancelled(true);
-    	}
+        if (player.hasPermission("safebuckets.debug")) {
+            plugin.debug(ChatColor.YELLOW + player.getName() +
+                         " clicked " + block.getType() + ":" + (int) block.getData() +
+                         " at " + Util.formatCoords(block));
+        }
+
+        if (plugin.getConfig().getBoolean("bucket.enabled")) {
+            if (plugin.getConfig().getBoolean("bucket.safe")) {
+                ItemStack itemInHand = player.getEquipment().getItemInMainHand().clone();
+                if (EnchantGlow.hasGlow(itemInHand)) {
+                    plugin.removeSafeLiquidFromCacheAndDB(block);
+                    if (player.getGameMode() == GameMode.SURVIVAL && player.hasPermission("safebuckets.tools.norefill")) {
+                        ItemStack resultBucket = new ItemStack(event.getBucket());
+                        EnchantGlow.addGlow(resultBucket);
+                        // https://hub.spigotmc.org/jira/projects/SPIGOT/issues/SPIGOT-2560
+                        // PlayerBucketEmptyEvent.setItemStack() does not affect
+                        // player's item in hand. Work around using task.
+                        // event.setItemStack(resultBucket);
+                        int currentSlot = player.getInventory().getHeldItemSlot();
+                        Bukkit.getScheduler().runTask(plugin, () -> {
+                            PlayerInventory inv = player.getInventory();
+                            if (inv.getHeldItemSlot() == currentSlot) {
+                                ItemStack item = inv.getItemInMainHand();
+                                if (item != null && item.getType() == Material.BUCKET) {
+                                    inv.setItemInMainHand(resultBucket);
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    plugin.addBlockToCacheAndDB(block);
+                }
+            }
+        } else {
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
@@ -243,9 +277,13 @@ public class SafeBucketsListener implements Listener {
         boolean isSafe = plugin.isSafeLiquid(block);
 
         // UI niceness to enable some actions in flow mode
-        if (event.hasItem() && event.getItem().getType().isEdible()) return; // Eat
-        if (event.hasBlock() && !player.getItemInHand().getType().equals(Material.AIR) && !event.getAction().equals(Action.LEFT_CLICK_BLOCK)) return; // Place blocks
-
+        if (event.hasItem() && event.getItem().getType().isEdible()) {
+            return; // Eat
+        }
+        if (event.hasBlock() && !player.getEquipment().getItemInMainHand().getType().equals(Material.AIR) &&
+            !event.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+            return; // Place blocks
+        }
         if (isLiquid && isSafe) {
             event.setCancelled(true);
 
@@ -266,13 +304,16 @@ public class SafeBucketsListener implements Listener {
             String type = block.getType().toString().toLowerCase();
             player.playSound(player.getLocation(), Sound.UI_BUTTON_CLICK, 1.0f, 1.0f);
             player.sendMessage(String.format("%sFlowed %s block at %d,%d,%d.", ChatColor.DARK_AQUA, type, block.getX(), block.getY(), block.getZ()));
-            plugin.getLogger().info(String.format("%s flowed %s block at %d,%d,%d.", player.getName(), type, block.getX(), block.getY(), block.getZ()));
+            plugin.getLogger().info(String.format("%s flowed %s block at %d,%d,%d.",
+                                                  player.getName(), type, block.getX(), block.getY(), block.getZ()));
         }
 
     }
 
     private boolean isPlayerFlowPermitted(Player player, Block block) {
-        if (plugin.getWG() == null) return false;
+        if (plugin.getWG() == null) {
+            return false;
+        }
         RegionManager regions = plugin.getWG().getRegionManager(block.getWorld());
         LocalPlayer wgPlayer = plugin.getWG().wrapPlayer(player);
         if (regions != null) {
@@ -285,5 +326,4 @@ public class SafeBucketsListener implements Listener {
         }
         return false;
     }
-
 }
