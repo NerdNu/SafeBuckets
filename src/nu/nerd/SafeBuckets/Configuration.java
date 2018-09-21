f3e9a128c0df239d3cb0ef611a656d389e22883f
bermudalocket <bermudalckt@gmail.com>
2018-09-21 15:29:10 -0400

    Removed configuration Keys constant in favor of String constants

--- src/nu/nerd/SafeBuckets/Configuration.java
+++ src/nu/nerd/SafeBuckets/Configuration.java
@@ -110,29 +110,29 @@ class Configuration {
         SafeBuckets.PLUGIN.saveDefaultConfig();
         _config = SafeBuckets.PLUGIN.getConfig();
 
-        TOOL_ITEM = getMaterial(Keys.TOOL_ITEM.getKey(), Material.BLAZE_ROD);
+        TOOL_ITEM = getMaterial("tools.inspection-tool", Material.BLAZE_ROD);
 
-        PREVENT_ICE_MELT = _config.getBoolean(Keys.PREVENT_ICE_MELT.getKey(), true);
+        PREVENT_ICE_MELT = _config.getBoolean("physics.prevent-ice-melt", true);
 
-        PLAYER_SELF_FLOW = _config.getBoolean(Keys.PLAYER_SELF_FLOW.getKey(), true);
+        PLAYER_SELF_FLOW = _config.getBoolean("player-self-flow.enabled", true);
         try {
-            String flowMode = _config.getString(Keys.PLAYER_SELF_FLOW_MODE.getKey(), "OWNER");
+            String flowMode = _config.getString("player-self-flow.mode");
             PLAYER_SELF_FLOW_MODE = PlayerFlowMode.valueOf(flowMode);
         } catch (Exception e) {
             PLAYER_SELF_FLOW_MODE = PlayerFlowMode.OWNER;
         }
-        PLAYER_SELF_FLOW_DURATION = _config.getInt(Keys.PLAYER_SELF_FLOW_DURATION.getKey(), 300) * 1000;
-        PLAYER_SELF_FLOW_CACHE_REVIEW_PERIOD = _config.getInt(Keys.PLAYER_SELF_FLOW_CACHE_REVIEW_PERIOD.getKey(), 30) * Util.TPS;
+        PLAYER_SELF_FLOW_DURATION = _config.getInt("player-self-flow.duration-in-seconds", 300) * 1000;
+        PLAYER_SELF_FLOW_CACHE_REVIEW_PERIOD = _config.getInt("player-self-flow.cache-review-period-in-seconds", 30) * Util.TPS;
 
-        DISPENSERS_ENABLED = _config.getBoolean(Keys.DISPENSERS_ALLOWED.getKey(), true);
-        DISPENSERS_SAFE = _config.getBoolean(Keys.DISPENSERS_SAFE.getKey(), true);
+        DISPENSERS_ENABLED = _config.getBoolean("dispensers.enabled", true);
+        DISPENSERS_SAFE = _config.getBoolean("dispensers.default-to-safe", true);
 
-        BUCKETS_ENABLED = _config.getBoolean(Keys.BUCKETS_ALLOWED.getKey(), true);
-        BUCKETS_SAFE = _config.getBoolean(Keys.BUCKETS_SAFE.getKey(), true);
+        BUCKETS_ENABLED = _config.getBoolean("tools.buckets-enabled", true);
+        BUCKETS_SAFE = _config.getBoolean("buckets.default-to-safe", true);
 
-        WORLDEDIT_HOOK = _config.getBoolean(Keys.WORLDEDIT_HOOK.getKey(), true);
-        WORLDEDIT_FLOWSEL_ENABLED = _config.getBoolean(Keys.WORLDEDIT_FLOWSEL_ENABLED.getKey(), true);
-        WORLDEDIT_FLOWSEL_MAX_BLOCKS = _config.getInt(Keys.WORLDEDIT_FLOWSEL_MAX_BLOCKS.getKey(), 100);
+        WORLDEDIT_HOOK = _config.getBoolean("worldedit.hook", true);
+        WORLDEDIT_FLOWSEL_ENABLED = _config.getBoolean("worldedit.flow-selection.enabled", true);
+        WORLDEDIT_FLOWSEL_MAX_BLOCKS = _config.getInt("worldedit.flow-selection.maxblocks", 100);
     }
 
     // ------------------------------------------------------------------------------------------------------
@@ -149,42 +149,5 @@ class Configuration {
         Material material = Material.getMaterial(materialName);
         return material != null ? material : def;
     }
-
-    // ------------------------------------------------------------------------------------------------------
-    /**
-     * The YAML keys (as strings) for each configuration option.
-     */
-    private enum Keys {
-
-        TOOL_ITEM("tools.inspection-tool"),
-
-        PREVENT_ICE_MELT("physics.prevent-ice-melt"),
-
-        BUCKETS_ALLOWED("buckets.enabled"),
-        BUCKETS_SAFE("buckets.default-to-safe"),
-
-        DISPENSERS_ALLOWED("dispensers.enabled"),
-        DISPENSERS_SAFE("dispensers.default-to-safe"),
-
-        WORLDEDIT_HOOK("worldedit.hook"),
-        WORLDEDIT_FLOWSEL_ENABLED("worldedit.flow-selection.enabled"),
-        WORLDEDIT_FLOWSEL_MAX_BLOCKS("worldedit.flow-selection.maxblocks"),
-
-        PLAYER_SELF_FLOW("player-self-flow.enabled"),
-        PLAYER_SELF_FLOW_MODE("player-self-flow.mode"),
-        PLAYER_SELF_FLOW_DURATION("player-self-flow.duration-in-seconds"),
-        PLAYER_SELF_FLOW_CACHE_REVIEW_PERIOD("player-self-flow.cache-review-period-in-seconds");
-
-        private String _key;
-
-        Keys(String key) {
-            _key = key;
-        }
-
-        public String getKey() {
-            return _key;
-        }
-
-    } // Keys
 
 } // Configuration
