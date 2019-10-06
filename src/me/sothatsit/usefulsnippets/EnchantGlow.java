3ec14cce67a1c1c73bf9ef69e36362f279fa0a99
totemo <TotemoResearch@gmail.com>
2019-10-06 21:54:32 +1030

    Return a non-null EnchantmentTarget, fixing RecipeManager NPE. Tabs->Spaces.

--- src/me/sothatsit/usefulsnippets/EnchantGlow.java
+++ src/me/sothatsit/usefulsnippets/EnchantGlow.java
@@ -9,69 +9,69 @@ import java.lang.reflect.Field;
 
 public class EnchantGlow extends EnchantmentWrapper {
 
-	private static Enchantment glow;
+    private static Enchantment glow;
 
-	private EnchantGlow(String name) {
-	    super(name);
-	}
+    private EnchantGlow(String name) {
+        super(name);
+    }
 
-	@Override
-	public boolean canEnchantItem(ItemStack item) {
-	    return true;
-	}
+    @Override
+    public boolean canEnchantItem(ItemStack item) {
+        return true;
+    }
 
-	@Override
-	public boolean conflictsWith(Enchantment other) {
-	    return false;
-	}
+    @Override
+    public boolean conflictsWith(Enchantment other) {
+        return false;
+    }
 
-	@Override
-	public EnchantmentTarget getItemTarget() {
-	    return null;
-	}
+    @Override
+    public EnchantmentTarget getItemTarget() {
+        return EnchantmentTarget.ALL;
+    }
 
-	@Override
-	public int getMaxLevel() {
-	    return 10;
-	}
+    @Override
+    public int getMaxLevel() {
+        return 10;
+    }
 
-	@Override
-	public String getName() {
-		return "Glow";
-	}
+    @Override
+    public String getName() {
+        return "Glow";
+    }
 
-	@Override
-	public int getStartLevel() {
-		return 1;
-	}
+    @Override
+    public int getStartLevel() {
+        return 1;
+    }
 
-	public static Enchantment getGlow() {
-		if (glow != null) {
-			return glow;
-		}
+    public static Enchantment getGlow() {
+        if (glow != null) {
+            return glow;
+        }
 
-		try {
-			Field f = Enchantment.class.getDeclaredField("acceptingNew");
-			f.setAccessible(true);
-			f.set(null, true);
-		} catch (Exception e) {
-			// e.printStackTrace();
-		}
+        try {
+            Field f = Enchantment.class.getDeclaredField("acceptingNew");
+            f.setAccessible(true);
+            f.set(null, true);
+        } catch (Exception e) {
+            // e.printStackTrace();
+        }
 
-		glow = new EnchantGlow("glow");
-		Enchantment.registerEnchantment(glow);
-		return glow;
-	}
+        glow = new EnchantGlow("glow");
+        Enchantment.registerEnchantment(glow);
+        return glow;
+    }
 
-	public static void addGlow(ItemStack item) {
-		Enchantment glow = getGlow();
-		item.addEnchantment(glow, 1);
-	}
+    public static void addGlow(ItemStack item) {
+        Enchantment glow = getGlow();
+        item.addEnchantment(glow, 1);
+    }
 
-	public static boolean hasGlow(ItemStack item) {
-		if (item == null || getGlow() == null || item.getEnchantments() == null) {
-			return false;
-		}
-		return item.getEnchantments().containsKey(getGlow());
-	}
+    public static boolean hasGlow(ItemStack item) {
+        if (item == null || getGlow() == null || item.getEnchantments() == null) {
+            return false;
+        }
+        return item.getEnchantments().containsKey(getGlow());
+    }
 }
